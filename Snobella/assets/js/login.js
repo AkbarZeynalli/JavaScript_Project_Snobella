659a71270fb31673926f1c3362a256e70263b8bf
Ekber Zeynalli <akbarhz-af106@code.edu.az>
2024-12-02 21:15:09 +0400

    procekt structure changed and push

--- Snobella/assets/js/login.js
+++ Snobella/assets/js/login.js
@@ -1,38 +1,38 @@
 let users = [
   {
     id: 1,
-    username: "gurban",
-    password: "123",
+    username: "akbar",
+    password: "12345",
   },
   {
     id: 2,
-    username: "gulchin",
-    password: "gulcin12",
+    username: "selim",
+    password: "1234",
   },
   {
-      id: 3,
-      username: "ali",
-      password: "a12",
-    },
+    id: 3,
+    username: "togrul",
+    password: "12",
+  },
 ];
 
 const page = JSON.parse(localStorage.getItem("isLogin"));
 
-const logForm= document.querySelector(".logForm");
-const userName= document.createElement('input')
-const passWord= document.createElement('input')
-const submits= document.createElement('button')
+const logForm = document.querySelector(".logForm");
+const userName = document.createElement('input')
+const passWord = document.createElement('input')
+const submits = document.createElement('button')
 
-userName.className=(".userName")
-passWord.className=(".passWord")
-submits.className=(".submits")
+userName.className = (".userName")
+passWord.className = (".passWord")
+submits.className = (".submits")
 
-userName.placeholder=" Username"
-passWord.placeholder=" Password"
-submits.textContent=" Submit"
+userName.placeholder = " Username"
+passWord.placeholder = " Password"
+submits.textContent = " Submit"
 
 
-logForm.append(userName,passWord,submits);
+logForm.append(userName, passWord, submits);
 //   console.log(submits);
 
 logForm.addEventListener("submit", (e) => {
@@ -48,6 +48,6 @@ logForm.addEventListener("submit", (e) => {
     localStorage.setItem("isLogin", JSON.stringify(true));
     window.location.href = "index.html";
   } else {
-   alert("Username ve passwordunuzu daxil edin!")
+    alert("Username ve passwordunuzu daxil edin!")
   }
 });
